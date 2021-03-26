5fae6e9f29cdd0632bafe2da3b36e23c7f3ec003
Michael Vazquez <mvazquez@wovenware.com>
2021-03-26 01:22:35 -0400

    added responsive header for small media

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Breadcrumbs, Link } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import {
+  Typography,
+  Breadcrumbs,
+  Link,
+  useMediaQuery,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -8,6 +17,9 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     display: "flex",
     justifyContent: "flex-end",
+  },
+  icon: {
+    marginRight: theme.spacing(1),
   },
   link: {
     "&:hover": {
@@ -23,7 +35,6 @@ const useStyles = makeStyles((theme) => ({
       color: "black",
       backgroundColor: "white",
       cursor: "pointer",
-      textDecoration: "none",
     },
   },
 }));
@@ -31,51 +42,84 @@ const useStyles = makeStyles((theme) => ({
 function Header(props) {
   const classes = useStyles();
 
+  const theme = useTheme();
+  const smallMedia = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   const handleClick = (page) => {
-    console.log(page);
     props.handlePageChange(page);
+    setAnchorEl(null);
+  };
+
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <div className={classes.header}>
-      <Breadcrumbs separator=" " aria-label="breadcrumb">
-        <Link
-          color="inherit"
-          onClick={() => handleClick(0)}
-          className={classes.link}
-        >
-          <Typography variant="subtitle1" className={classes.items}>
-            About Me
-          </Typography>
-        </Link>
-        <Link
-          color="inherit"
-          onClick={() => handleClick(1)}
-          className={classes.link}
-        >
-          <Typography variant="subtitle1" className={classes.items}>
-            Skills
-          </Typography>
-        </Link>
-        <Link
-          color="inherit"
-          onClick={() => handleClick(2)}
-          className={classes.link}
-        >
-          <Typography variant="subtitle1" className={classes.items}>
-            Experience
-          </Typography>
-        </Link>
-        <Link
-          color="inherit"
-          onClick={() => handleClick(3)}
-          className={classes.link}
-        >
-          <Typography variant="subtitle1" className={classes.items}>
-            Contact Me
-          </Typography>
-        </Link>
-      </Breadcrumbs>
+      {smallMedia ? (
+        <>
+          <IconButton className={classes.icon} onClick={openMenu}>
+            <MenuRoundedIcon fontSize="large" />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={() => handleClick(0)}>About Me</MenuItem>
+            <MenuItem onClick={() => handleClick(1)}>Skills</MenuItem>
+            <MenuItem onClick={() => handleClick(2)}>Experience</MenuItem>
+            <MenuItem onClick={() => handleClick(3)}>Contact Me</MenuItem>
+          </Menu>
+        </>
+      ) : (
+        <Breadcrumbs separator=" " aria-label="breadcrumb">
+          <Link
+            color="inherit"
+            onClick={() => handleClick(0)}
+            className={classes.link}
+          >
+            <Typography variant="subtitle1" className={classes.items}>
+              About Me
+            </Typography>
+          </Link>
+          <Link
+            color="inherit"
+            onClick={() => handleClick(1)}
+            className={classes.link}
+          >
+            <Typography variant="subtitle1" className={classes.items}>
+              Skills
+            </Typography>
+          </Link>
+          <Link
+            color="inherit"
+            onClick={() => handleClick(2)}
+            className={classes.link}
+          >
+            <Typography variant="subtitle1" className={classes.items}>
+              Experience
+            </Typography>
+          </Link>
+          <Link
+            color="inherit"
+            onClick={() => handleClick(3)}
+            className={classes.link}
+          >
+            <Typography variant="subtitle1" className={classes.items}>
+              Contact Me
+            </Typography>
+          </Link>
+        </Breadcrumbs>
+      )}
     </div>
   );
 }
